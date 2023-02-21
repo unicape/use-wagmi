92e0d634834e0203510e0c450a3dbd7b9cc983dc
Unic Ape <94770222@qq.com>
2023-02-21 18:44:27 +0800

    feat(useAccount): add demo

---
+++ src/core/accounts/useAccount/demo.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>status: {{ status }}</p>
+    <p>address: {{ address }}</p>
+    <p>isConnecting: {{ isConnecting }}</p>
+    <p>isConnected: {{ isConnected }}</p>
+    <p>isReconnecting: {{ isReconnecting }}</p>
+    <p>isDisconnected: {{ isDisconnected }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useAccount } from 'use-wagmi/core'
+
+const {
+  status,
+  address,
+  isConnecting,
+  isConnected,
+  isReconnecting,
+  isDisconnected,
+  connector
+} = useAccount({
+  onConnect (data) {
+    // onConnect
+  },
+  onDisconnect () {
+    // onDisconnect
+  }
+})
+
+console.log(connector);
+</script>
