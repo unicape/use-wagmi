73a18035c4cab3bfaab77540a6a4b889977f5d14
Unic Ape <94770222@qq.com>
2023-03-06 14:21:00 +0800

    feat: providers hooks

---
+++ src/core/providers/useWebSocketProvider/index.ts
@@ -0,0 +1,28 @@
+import { ref, unref, readonly, markRaw, watchEffect } from 'vue-demi'
+import { getWebSocketProvider, watchWebSocketProvider } from '@wagmi/core'
+
+import type { GetWebSocketProviderArgs, WebSocketProvider } from '@wagmi/core'
+import type { SetMaybeRef } from '../../../types'
+
+export type UseWebsocketProviderArgs = SetMaybeRef<Partial<GetWebSocketProviderArgs>>
+
+export function useWebSocketProvider<
+  TWebSocketProvider extends WebSocketProvider
+> ({
+  chainId
+}: UseWebsocketProviderArgs = {}) {
+  const webSocketProvider = ref(markRaw(getWebSocketProvider<TWebSocketProvider>({ chainId: unref(chainId) }) || {}))
+
+  watchEffect(onInvalidate => {
+    const unwatch = watchWebSocketProvider<TWebSocketProvider>(
+      { chainId: unref(chainId) },
+      (webSocketProvider_) => {
+        webSocketProvider.value = markRaw(webSocketProvider_ || {})
+      }
+    )
+
+    onInvalidate(() => unwatch())
+  })
+
+  return readonly(webSocketProvider)
+}
