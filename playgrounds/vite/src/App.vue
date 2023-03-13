2657bd48d2201ff928339c47f7d174b25d9c3419
Unic Ape <94770222@qq.com>
2023-03-13 17:07:31 +0800

    chore: playground

--- playgrounds/vite/src/App.vue
+++ playgrounds/vite/src/App.vue
@@ -1,33 +1,9 @@
 <template>
-  <div>
-    <p>status: {{ status }}</p>
-    <p>address: {{ address }}</p>
-    <p>isConnecting: {{ isConnecting }}</p>
-    <p>isConnected: {{ isConnected }}</p>
-    <p>isReconnecting: {{ isReconnecting }}</p>
-    <p>isDisconnected: {{ isDisconnected }}</p>
-  </div>
+  <Connect />
+  <Account />
 </template>
 
 <script lang="ts" setup>
-import { useAccount } from 'use-wagmi/core'
-
-const {
-  status,
-  address,
-  isConnecting,
-  isConnected,
-  isReconnecting,
-  isDisconnected,
-  connector
-} = useAccount({
-  onConnect (data) {
-    // onConnect
-  },
-  onDisconnect () {
-    // onDisconnect
-  }
-})
-
-console.log(connector);
+import Connect from '@/components/connect.vue'
+import Account from '@/components/account.vue'
 </script>
--- playgrounds/vite/vite.config.ts
+++ playgrounds/vite/vite.config.ts
@@ -5,8 +5,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      }
+    }
+  },
   resolve: {
     alias: {
+      '@': resolve(__dirname, './src'),
       'use-wagmi': resolve(__dirname, '../../src'),
       'use-wagmi/*': resolve(__dirname, '../../src/*'),
     },
