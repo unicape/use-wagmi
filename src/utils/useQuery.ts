8f534387c8de35c13cf3937f214b2b17fa85e0f2
Unic Ape <94770222@qq.com>
2023-03-21 18:27:45 +0800

    fix(useQuery): type

--- src/utils/useQuery.ts
+++ src/utils/useQuery.ts
@@ -1,8 +1,32 @@
 import { useQuery as useBaseQuery } from 'vue-query'
 import { WagmiQueryClientKey as queryClientKey } from '../create'
 
-import type { UnwrapRef } from 'vue-demi'
-import type { QueryKey, QueryFunction, UseQueryOptions } from 'vue-query'
+import type { ToRefs, UnwrapRef } from 'vue-demi'
+import type {
+  QueryKey,
+  QueryFunction,
+  UseQueryOptions,
+  QueryObserverResult,
+  DefinedQueryObserverResult,
+  UseQueryReturnType as UQRT
+} from 'vue-query'
+
+type UseQueryReturnType<TData, TError> = Omit<
+  UQRT<TData, TError>,
+  "refetch" | "remove"
+> & {
+  refetch: QueryObserverResult<TData, TError>["refetch"];
+  remove: QueryObserverResult<TData, TError>["remove"];
+}
+
+type UseQueryDefinedReturnType<TData, TError> = Omit<
+  ToRefs<Readonly<DefinedQueryObserverResult<TData, TError>>>,
+  "refetch" | "remove"
+> & {
+  suspense: () => Promise<QueryObserverResult<TData, TError>>;
+  refetch: QueryObserverResult<TData, TError>["refetch"];
+  remove: QueryObserverResult<TData, TError>["remove"];
+}
 
 export function useQuery<
   TQueryFnData,
@@ -13,7 +37,9 @@ export function useQuery<
   queryKey: TQueryKey,
   queryFn: QueryFunction<TQueryFnData, UnwrapRef<TQueryKey>>,
   options: UseQueryOptions<TQueryFnData, TError, TData, TQueryKey>
-) {
+):
+| UseQueryReturnType<TData, TError>
+| UseQueryDefinedReturnType<TData, TError> {
   return useBaseQuery({
     queryKey,
     queryFn,
