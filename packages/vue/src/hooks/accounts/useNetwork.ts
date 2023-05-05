6678a7d4e646590dca746a0011754f7ab07227db
Unic Ape <94770222@qq.com>
2023-05-05 19:18:49 +0800

    fix(useNetwork): result type

--- packages/vue/src/hooks/accounts/useNetwork.ts
+++ packages/vue/src/hooks/accounts/useNetwork.ts
@@ -1,5 +1,13 @@
 import { getNetwork, watchNetwork } from '@wagmi/core'
+import type { Chain } from '@wagmi/core'
 import { getCurrentScope, onScopeDispose, reactive, toRefs } from 'vue-demi'
+
+type GetNetworkResult = {
+  chain?: Chain & {
+    unsupported?: boolean
+  }
+  chains: Chain[]
+}
 
 export function useNetwork() {
   let network = reactive(getNetwork())
@@ -10,5 +18,5 @@ export function useNetwork() {
 
   if (getCurrentScope()) onScopeDispose(() => unwatch())
 
-  return toRefs(network)
+  return toRefs<GetNetworkResult>(network)
 }
