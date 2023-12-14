3597dd68c9e7730243cfc97734991eb08efc84e4
Unic Ape <unicape.dev@gmail.com>
2023-12-14 11:41:57 +0800

    feat: 2.7 examples

---
+++ examples/vue2.7/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    {{ address }}
+
+    <button @click="connect">Connect</button>
+    <button @click="disconnect">Disconnect</button>
+  </div>
+</template>
+
+<script setup>
+import { useAccount, useConnect, useDisconnect } from 'use-wagmi'
+import { InjectedConnector } from 'use-wagmi/connectors/injected'
+
+const { address } = useAccount()
+const { connect } = useConnect({ connector: new InjectedConnector() })
+const { disconnect } = useDisconnect()
+</script>
